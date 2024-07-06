da1c570bf19a85b59b4956f05ad2823af634ba15
gandhiIskandar <alfathgandhi98@Gmail.com>
2024-07-06 15:25:38 +0700

    update noindex

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -5,6 +5,7 @@
   <head>
     <title>Login Data Player</title>
     <!-- [Meta] -->
+    <meta name="robots" content="noindex">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
